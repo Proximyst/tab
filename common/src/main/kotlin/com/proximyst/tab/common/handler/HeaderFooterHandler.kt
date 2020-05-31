b99b7926251e0c319ab397c656ee66ca04b5b16a
Mariell Hoversholm <proximyst@proximyst.com>
2020-05-31 13:39:51 +0200

    Update licence

--- common/src/main/kotlin/com/proximyst/tab/common/handler/HeaderFooterHandler.kt
+++ common/src/main/kotlin/com/proximyst/tab/common/handler/HeaderFooterHandler.kt
@@ -1,3 +1,20 @@
+/**
+ * tab - A plugin for tab manipulation with ease in mind.
+ * Copyright (C) 2020 Mariell Hoversholm
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 package com.proximyst.tab.common.handler
 
 import com.proximyst.tab.common.IPlaceholderApi
